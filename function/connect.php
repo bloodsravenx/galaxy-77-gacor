5ffab61a945d3454438ac65f00500472c04865d6
fedryn <122414332+bloodsravenx@users.noreply.github.com>
2026-02-22 00:15:03 +0700

    Update connect.php

--- function/connect.php
+++ function/connect.php
@@ -5,10 +5,11 @@ error_reporting(0);
 date_default_timezone_set('Asia/Jakarta');
 
 $host 			= "localhost";
-$user 			= "USER_DATABASE";
-$password 		= "PASS_DATABASE";
-$database 		= "NAMA_DATABASE";
+$user 			= "mimintop";
+$password 		= "mimintop";
+$database 		= "database";
 
 $koneksi = mysqli_connect($host, $user, $password, $database) or die(mysqli_error());
+
 
 ?>
